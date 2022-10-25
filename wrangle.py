08fc19bcbe715f0230997f5c9a785b4ba2ea64a3
Mark Foster <markdouglasfoster@gmail.com>
2022-10-25 17:03:24 -0500

    Add split function'

--- wrangle.py
+++ wrangle.py
@@ -59,3 +59,65 @@ def get_telco_data():
         df.to_csv(filename,index=False)
         
     return df
+
+###################################################################################
+##################################### PREP DATA ###################################
+###################################################################################
+
+def prep_telco(df):
+    '''Prepares acquired teclo data for exploration'''
+
+    df.drop_duplicates(inplace=True)
+    df.drop(columns=['customer_id','payment_type_id', 'internet_service_type_id','contract_type_id'], inplace=True)
+    df = df[df.total_charges!=' ']
+    df.total_charges = df.total_charges.astype(float)
+    
+    return df
+
+
+###################################################################################
+#################################### SPLIT DATA ###################################
+###################################################################################
+
+def train_validate_test_split(df, target):
+    '''
+    Takes in a data frame and the target variable column  and returns
+    train (75%), validate (15%), and test (10%) data frames.
+    '''
+    train, test = train_test_split(df,test_size = 0.1, stratify = df[target], random_state=27)
+    train, validate = train_test_split(train, test_size = 0.166666666666, stratify = train[target],random_state=27)
+    
+    return train, validate, test
+
+def prep_for_model(train, validate, test, target):
+    '''
+    Takes in train, validate, and test data frames
+    then splits  for X (all variables but target variable) 
+    and y (only target variable) for each data frame
+    '''
+    drop_columns = list(train.select_dtypes(exclude=np.number).columns) + [target]
+
+    X_train = train.drop(columns=drop_columns)
+    y_train = train[target]
+
+    X_validate = validate.drop(columns=drop_columns)
+    y_validate = validate[target]
+
+    X_test = test.drop(columns=drop_columns)
+    y_test = test[target]
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
+
+def prep_df_for_model(df, target):
+    '''
+    Takes in a data frame and the target variable column, splits the data
+    into train (80%), validate (15%), and test (10%) data frames
+    then splits again for X (all variables but target variable) and 
+    y (only target variable) for each data frame
+    '''
+    
+    train, validate, test = train_validate_test_split(df, target)
+
+    X_train, y_train, X_validate, y_validate, X_test, y_test = prep_for_model(train, validate, test, target)
+
+    return X_train, y_train, X_validate, y_validate, X_test, y_test
